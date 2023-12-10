23df4e783a8dcdfaa2c5f2ae79ccb2d46534e444
Gleb Andronchyk <gandronchyk@ek-mb-086.local>
2023-12-10 14:49:56 +0200

    refactored vector class

--- src/utils/Vector.ts
+++ src/utils/Vector.ts
@@ -1,35 +1,31 @@
 export class Vector {
-  #x: number;
-  #y: number;
+  private x: number;
+  private y: number;
 
   constructor(x: number, y: number) {
-    this.#y = y;
-    this.#x = x;
+    this.y = y;
+    this.x = x;
   }
 
-  getX = () => this.#x;
-  getY = () => this.#y;
+  getX = () => this.x;
+  getY = () => this.y;
 
   setAngle(angle: number) {
     const length = this.getLength();
-    this.#x = Math.cos(angle) * length;
-    this.#y = Math.sin(angle) * length;
+    this.x = Math.cos(angle) * length;
+    this.y = Math.sin(angle) * length;
   }
-  getAngle = () => Math.atan2(this.#x, this.#y);
+  getAngle = () => Math.atan2(this.x, this.y);
 
   setLength(length: number) {
     const angle = this.getAngle();
-    this.#x = Math.cos(angle) * length;
-    this.#y = Math.sin(angle) * length;
+    this.x = Math.cos(angle) * length;
+    this.y = Math.sin(angle) * length;
   }
-  getLength = () => Math.sqrt(this.#x * this.#x + this.#y * this.#y);
+  getLength = () => Math.sqrt(this.x ** 2 + this.y ** 2);
 
   addTo(x: number, y: number) {
-    this.#x += x;
-    this.#y += y;
-  }
-
-  debuggerView() {
-    return [`x: ${this.#x}`, `y: ${this.#y}`];
+    this.x += x;
+    this.y += y;
   }
 }
